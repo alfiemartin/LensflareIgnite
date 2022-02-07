4c2553e1672753d2410bb8065bcf4fb4335c5e27
Alfie <alfie.martin@hotmail.co.uk>
2022-02-07 18:27:23 +0000

    building the layout

--- app/screens/profile/profile-screen.tsx
+++ app/screens/profile/profile-screen.tsx
@@ -4,6 +4,7 @@ import { ScrollView, TextStyle, View, ViewStyle } from "react-native"
 import { EasyIcon, ImageBox, Screen, Text } from "../../components"
 import { color, spacing } from "../../theme"
 import { useStores } from "../../models"
+import { Col, Grid } from "react-native-easy-grid"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.fullBlack,
@@ -16,6 +17,11 @@ const PROFILE_CONTAINER: ViewStyle = {
   paddingHorizontal: spacing[3],
   borderBottomLeftRadius: 20,
   borderBottomRightRadius: 20,
+}
+
+const HEADER: ViewStyle = {
+  flexDirection: "row",
+  justifyContent: "space-between",
 }
 
 const SCROLL_VIEW: ViewStyle = {
@@ -37,13 +43,20 @@ export const ProfileScreen = observer(function ProfileScreen() {
   return (
     <Screen style={ROOT}>
       <View style={PROFILE_CONTAINER}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={HEADER}>
           <Text preset="default" text="Alfie" style={TEXT} />
           <EasyIcon name="settings-sharp" size={40} color={color.primary} />
         </View>
         <ScrollView style={SCROLL_VIEW}>
-          <ImageBox image={profiles[0].image} text="Explorer Profile" />
-          <ImageBox image={profiles[1].image} text="Matches profile" />
+          <Grid>
+            <Col style={{ marginRight: 5 }}>
+              <ImageBox image={profiles[0].image} text="Friends" />
+            </Col>
+            <Col style={{ marginLeft: 5 }}>
+              <ImageBox image={profiles[1].image} text="Followers" />
+            </Col>
+          </Grid>
+          <ImageBox image={profiles[2].image} text="My Profile" />
         </ScrollView>
       </View>
     </Screen>
